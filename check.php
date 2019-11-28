004387a8602c4da1ec666b38c3c5dbf01981c524
間下善貴 <y.mashimo0105@gmail.com>
2019-11-28 16:55:36 +0900

    更新

---
+++ check.php
@@ -0,0 +1,63 @@
+<?php
+// 入力内容を取得
+$nickname = $_POST['nickname'];
+$email = $_POST['email'];
+$content = $_POST['content'];
+
+// 入力内容のチェック
+if ($nickname == '') {
+    $nickname_result = 'ニックネームが入力されていません。';
+} else {
+    $nickname_result = 'ようこそ、' . $nickname .'様';
+}
+
+if ($email == '') {
+    $email_result = 'メールアドレスが入力されていません。';
+} else {
+    $email_result = 'メールアドレス：' . $email;
+}
+
+if ($content == '') {
+    $content_result =  'お問い合わせ内容が入力されていません。';
+} else {
+    $content_result = 'お問い合わせ内容：' . $content;
+}
+
+    // // ニックネーム
+    // $nickname = $_POST['nickname'];
+    // echo $nickname . '<br>';
+    // echo '<br>';
+
+    // // メールアドレス
+    // $email = $_POST['email'];
+    // echo $email . '<br>';
+    // echo '<br>';
+
+    // // お問い合わせ
+    // $content = $_POST['content'];
+    // echo $content . '<br>';
+    // echo '<br>';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>入力内容確認</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>入力内容確認</h1>
+    <!-- 画面に表示 -->
+    <p><?php echo $nickname_result; ?></p>
+    <p><?php echo $email_result; ?></p>
+    <p><?php echo $content_result; ?></p>
+    <form method="POST" action="thanks.php">
+        <input type="hidden" name='nickname' value="<?= $nickname ?>">
+        <input type="hidden" name='email' value="<?= $email ?>">
+        <input type="hidden" name='content' value="<?= $content ?>">
+        <button type="button" onclick="history.back()">戻る</button>
+        <?php if ($nickname != '' && $email != '' && $content != '') : ?>
+            <input type="submit" value="OK">
+        <?php endif; ?>
+    </form>
+</body>
+</html>
---
+++ thanks.php
@@ -0,0 +1,27 @@
+<?php
+// 入力内容の取得
+$nickname = $_POST['nickname'];
+$email = $_POST['email'];
+$content = $_POST['content'];
+// 表示
+// echo $nickname . '<br>';
+// echo '<br>';
+// echo $email . '<br>';
+// echo '<br>';
+// echo $content . '<br>';
+// echo '<br>';
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>送信完了</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>お問い合わせありがとうございました！</h1>
+    <p><?php echo $nickname; ?></p>
+    <p><?php echo $email; ?></p>
+    <p><?php echo $content; ?></p>
+</body>
+</html>
